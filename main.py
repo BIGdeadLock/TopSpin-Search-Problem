526e41a405893f1ca7dda2a6e6ae1d5defeaa344
BigDeadLock <eden.yavin25@gmail.com>
2023-06-06 22:48:34 +0300

    wa with 5

--- main.py
+++ main.py
@@ -16,35 +16,13 @@ hard_problem = [1, 5, 11, 2, 6, 3, 9, 4, 10, 7, 8]  # hard instance
 result = pd.DataFrame(columns=['method', "heuristic", "runtime", "path_length", "expansions"])
 method_names = {
     "A*": f_priority,
-    "WA*": fw_priority(w=0.7),
+    "WA*": fw_priority(w=5),
     "GBFS": h_priority
 }
 heuristic_names = {
     "basic": BaseHeuristic,
     "advanced": AdvanceHeuristic
 }
-
-def find_best_w(goal_state: TopSpinState, heuristic) -> float:
-    """
-    Run experiments to determine the best weights for the WA* algorithm
-    :param goal_state: Topspin goal state
-    :param heuristic: what heuristic to run the algorithm with
-    :return: W the best weight
-    """
-    start_state = random_walks(goal_state, 50, 1)
-    start_state = TopSpinState(start_state[0], 4)
-    results = []
-    for weight in tqdm(np.arange(0.1, 0.9, 0.1), unit="weight"):
-        print(f"Using weight {weight}")
-        wa_star = fw_priority(w=weight)
-        start_time = time.time()
-        search(start_state, wa_star, heuristic(11, 4).get_h_value)
-        end_time = time.time()
-        results.append(end_time - start_time)
-
-    min_index = np.argmin(results)
-    best_weight = 0.1 * min_index
-    return best_weight
 
 
 def random_walks(start_state: TopSpinState, number_of_potential_walks, number_of_instances_to_create=50) -> list:
@@ -108,14 +86,11 @@ random.seed(42)  # Set a seed for reproducibility
 # Run 50 random solvable instances
 instances = []
 
-# w = find_best_w(goal_state=TopSpinState(np.array(range(1,12)), 4), heuristic=AdvanceHeuristic)
-# print(w)
-
 # Create the list of 50 solvable instances.
 print("Creating 50 solvable instances to solve")
-instances += random_walks(TopSpinState(np.array(range(1,12)), 4), number_of_potential_walks=1000)
+instances += random_walks(TopSpinState(np.array(range(1,12)), 4), number_of_potential_walks=1000, number_of_instances_to_create=20)
 
-for instance in instances:
+for instance in tqdm(instances):
     run_search(instance)
     print()  # Blank line
 
